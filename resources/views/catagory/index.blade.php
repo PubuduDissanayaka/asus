d88cd84463db2896a4c89f86b65933cbb2fc8268
unknown <pubududissanayaka14@gmail.com>
2019-01-24 00:37:47 +0530

    first commit

---
+++ resources/views/catagory/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <h1>Catagory index page</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo consequatur sit porro perspiciatis? Asperiores recusandae sapiente porro facere, ullam explicabo.</p>
+</div>
+
+<div class="container-fluid">
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio minima pariatur nam quibusdam accusamus enim odit animi autem doloribus dolorem possimus sapiente harum ad saepe deleniti maiores inventore dolore et, repellat at neque aliquid quas odio? Officia quas repellendus hic consequuntur nisi laudantium reiciendis esse nihil? Illum voluptatibus nihil assumenda!</p>
+</div>
+    
+@endsection
+
+
